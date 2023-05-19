10df2a9a8d693621540e5bdf154be45524d365eb
Sebastian Valle <79114580+Vallit0@users.noreply.github.com>
2023-05-18 22:16:54 -0600

    Update ejercicioUno.py

--- ejercicioUno.py
+++ ejercicioUno.py
@@ -1,32 +1,40 @@
-# Random 
+# Random
 import random
-# Main 
-def main():
 
+
+# Main
+def main():
     # Conjunto
-    A = ["a","b","c","d","e","f","g","h","i","j"]
+    A = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
     palabras = []
     palabra = ""
     # palabra que se debe crear de 6 elementos
-    counter = 0
+    comparacionesRepetidas = 0
     contadorGeneral = 0
 
-    while (counter <= 5):
+    while (comparacionesRepetidas <= 20000):
+        
+        #Generamos una palabra al azar
         for i in range(6):
-            palabra += A[random.randint(0,6)]
+            palabra += A[random.randint(0, 6)]
+        
+        # Si la palabra no fue generada antes entonces la añadimos
         if palabra not in palabras:
             palabras.append(palabra)
             print(palabra)
+            # Puesto que añadimos una nueva palabra, la contamos
             contadorGeneral += 1
+            
+            #Reset palabra
             palabra = ""
         else:
             palabra = ""
-            counter += 1
-    
+            comparacionesRepetidas += 1
+    print("--- Iteraciones Terminadas -----")
     print(contadorGeneral)
-            
+
+
 if __name__ == '__main__':
     main()
-    
 
-    
+
